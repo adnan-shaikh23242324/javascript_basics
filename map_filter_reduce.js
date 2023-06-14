b489209ee16483f88a56effa9f38e549fed8409d
adnan-shaikh23242324 <107952487+adnan-shaikh23242324@users.noreply.github.com>
2023-06-14 16:59:05 +0530

    learn map filter reduce method of javascript

--- map_filter_reduce.js
+++ map_filter_reduce.js
@@ -1,17 +1,17 @@
 let arr = [45, 23, 21];
 // Array map method
 let a = arr.map((value, index, array) => {
-  // console.log(value, index, array)
+  console.log(value, index, array)
   return value + index;
 });
-// console.log(arr)
+console.log(arr);
 
 // Array filter method
 let arr2 = [45, 23, 21, 0, 3, 5];
 let a2 = arr2.filter((a) => {
   return a < 10;
 });
-// console.log(a2, arr2)
+console.log(a2, arr2);
 
 // Array reduce method
 let arr3 = [1, 2, 3, 5, 2, 1];
